6b75a58a743a413ef97a5d727a9f8c609757f244
Colin Page <cwpage@umich.edu>
2018-11-19 21:27:30 -0500

    Fully linted code, added FAQ to README

--- sobelfilter/__main__.py
+++ sobelfilter/__main__.py
@@ -3,6 +3,7 @@ import math
 import os
 import numpy
 import imageio
+import click
 
 
 class SobelFilter:
@@ -15,9 +16,9 @@ class SobelFilter:
 
     def find_intensity(self, x, y):
         """Find intensity of a pixel."""
-        red = self.image[x,y,0]
-        green = self.image[x,y,1]
-        blue = self.image[x,y,2]
+        red = self.image[x, y, 0]
+        green = self.image[x, y, 1]
+        blue = self.image[x, y, 2]
 
         return red + green + blue
 
@@ -27,98 +28,98 @@ class SobelFilter:
         g_y = 0
 
         if x == 0 and y == 0:
-            g_y += 2 * self.find_intensity(x,y+1)
+            g_y += 2 * self.find_intensity(x, y+1)
 
-            g_x += 2 * self.find_intensity(x+1,y)
+            g_x += 2 * self.find_intensity(x+1, y)
 
-            g_x += self.find_intensity(x+1,y+1)
-            g_y += self.find_intensity(x+1,y+1)
+            g_x += self.find_intensity(x+1, y+1)
+            g_y += self.find_intensity(x+1, y+1)
         elif x == 0 and y == (self.image.shape[1] - 1):
-            g_y += -2 * self.find_intensity(x,y-1)
+            g_y += -2 * self.find_intensity(x, y-1)
 
-            g_x += self.find_intensity(x+1,y-1)
-            g_y += -(self.find_intensity(x+1,y-1))
+            g_x += self.find_intensity(x+1, y-1)
+            g_y += -(self.find_intensity(x+1, y-1))
 
-            g_x += 2 * self.find_intensity(x+1,y)
+            g_x += 2 * self.find_intensity(x+1, y)
         elif x == (self.image.shape[0] - 1) and y == 0:
-            g_x += -2 * self.find_intensity(x-1,y)
+            g_x += -2 * self.find_intensity(x-1, y)
 
-            g_y += 2 * self.find_intensity(x,y+1)
+            g_y += 2 * self.find_intensity(x, y+1)
         elif x == (self.image.shape[0] - 1) and y == (self.image.shape[1] - 1):
-            g_x += -(self.find_intensity(x-1,y-1))
-            g_y += -(self.find_intensity(x-1,y-1))
+            g_x += -(self.find_intensity(x-1, y-1))
+            g_y += -(self.find_intensity(x-1, y-1))
 
-            g_x += -2 * self.find_intensity(x-1,y)
+            g_x += -2 * self.find_intensity(x-1, y)
 
-            g_y += -2 * self.find_intensity(x,y-1)
+            g_y += -2 * self.find_intensity(x, y-1)
         elif x == 0:
-            g_y += -2 * self.find_intensity(x,y-1)
+            g_y += -2 * self.find_intensity(x, y-1)
 
-            g_y += 2 * self.find_intensity(x,y+1)
+            g_y += 2 * self.find_intensity(x, y+1)
 
-            g_x += self.find_intensity(x+1,y-1)
-            g_y += -(self.find_intensity(x+1,y-1))
+            g_x += self.find_intensity(x+1, y-1)
+            g_y += -(self.find_intensity(x+1, y-1))
 
-            g_x += 2 * self.find_intensity(x+1,y)
+            g_x += 2 * self.find_intensity(x+1, y)
 
-            g_x += self.find_intensity(x+1,y+1)
-            g_y += self.find_intensity(x+1,y+1)
+            g_x += self.find_intensity(x+1, y+1)
+            g_y += self.find_intensity(x+1, y+1)
         elif y == 0:
-            g_x += -2 * self.find_intensity(x-1,y)
+            g_x += -2 * self.find_intensity(x-1, y)
 
             g_x += -(self.find_intensity(x-1, y+1))
-            g_y += self.find_intensity(x-1,y+1)
+            g_y += self.find_intensity(x-1, y+1)
 
-            g_y += 2 * self.find_intensity(x,y+1)
+            g_y += 2 * self.find_intensity(x, y+1)
 
-            g_x += 2 * self.find_intensity(x+1,y)
+            g_x += 2 * self.find_intensity(x+1, y)
 
-            g_x += self.find_intensity(x+1,y+1)
-            g_y += self.find_intensity(x+1,y+1)
+            g_x += self.find_intensity(x+1, y+1)
+            g_y += self.find_intensity(x+1, y+1)
         elif x == (self.image.shape[0] - 1):
-            g_x += -(self.find_intensity(x-1,y-1))
-            g_y += -(self.find_intensity(x-1,y-1))
+            g_x += -(self.find_intensity(x-1, y-1))
+            g_y += -(self.find_intensity(x-1, y-1))
 
-            g_x += -2 * self.find_intensity(x-1,y)
+            g_x += -2 * self.find_intensity(x-1, y)
 
             g_x += -(self.find_intensity(x-1, y+1))
-            g_y += self.find_intensity(x-1,y+1)
+            g_y += self.find_intensity(x-1, y+1)
 
-            g_y += -2 * self.find_intensity(x,y-1)
+            g_y += -2 * self.find_intensity(x, y-1)
 
-            g_y += 2 * self.find_intensity(x,y+1)
+            g_y += 2 * self.find_intensity(x, y+1)
         elif y == (self.image.shape[1] - 1):
-            g_x += -(self.find_intensity(x-1,y-1))
-            g_y += -(self.find_intensity(x-1,y-1))
+            g_x += -(self.find_intensity(x-1, y-1))
+            g_y += -(self.find_intensity(x-1, y-1))
 
-            g_x += -2 * self.find_intensity(x-1,y)
+            g_x += -2 * self.find_intensity(x-1, y)
 
-            g_y += -2 * self.find_intensity(x,y-1)
+            g_y += -2 * self.find_intensity(x, y-1)
 
-            g_x += self.find_intensity(x+1,y-1)
-            g_y += -(self.find_intensity(x+1,y-1))
+            g_x += self.find_intensity(x+1, y-1)
+            g_y += -(self.find_intensity(x+1, y-1))
 
-            g_x += 2 * self.find_intensity(x+1,y)
+            g_x += 2 * self.find_intensity(x+1, y)
         else:
-            g_x += -(self.find_intensity(x-1,y-1))
-            g_y += -(self.find_intensity(x-1,y-1))
+            g_x += -(self.find_intensity(x-1, y-1))
+            g_y += -(self.find_intensity(x-1, y-1))
 
-            g_x += -2 * self.find_intensity(x-1,y)
+            g_x += -2 * self.find_intensity(x-1, y)
 
             g_x += -(self.find_intensity(x-1, y+1))
-            g_y += self.find_intensity(x-1,y+1)
+            g_y += self.find_intensity(x-1, y+1)
 
-            g_y += -2 * self.find_intensity(x,y-1)
+            g_y += -2 * self.find_intensity(x, y-1)
 
-            g_y += 2 * self.find_intensity(x,y+1)
+            g_y += 2 * self.find_intensity(x, y+1)
 
-            g_x += self.find_intensity(x+1,y-1)
-            g_y += -(self.find_intensity(x+1,y-1))
+            g_x += self.find_intensity(x+1, y-1)
+            g_y += -(self.find_intensity(x+1, y-1))
 
-            g_x += 2 * self.find_intensity(x+1,y)
+            g_x += 2 * self.find_intensity(x+1, y)
 
-            g_x += self.find_intensity(x+1,y+1)
-            g_y += self.find_intensity(x+1,y+1)
+            g_x += self.find_intensity(x+1, y+1)
+            g_y += self.find_intensity(x+1, y+1)
 
         return (g_x, g_y)
 
@@ -126,7 +127,7 @@ class SobelFilter:
         """Filter image into filtered_image."""
         for x in range(0, self.image.shape[0]):
             for y in range(0, self.image.shape[1]):
-                g_xy_pair = self.get_gx_gy(x,y)
+                g_xy_pair = self.get_gx_gy(x, y)
                 g_x = g_xy_pair[0]
                 g_y = g_xy_pair[1]
                 gradient_length = math.sqrt((g_x * g_x) + (g_y * g_y))
@@ -134,11 +135,10 @@ class SobelFilter:
                 normalized_length = gradient_length / 4328 * 255
                 normalized_length = int(normalized_length)
 
-                for i in range(0,3):
-                    self.filtered_image[x,y,i] = normalized_length
+                for i in range(0, 3):
+                    self.filtered_image[x, y, i] = normalized_length
 
         self.filtered_image = self.filtered_image.astype(numpy.uint8)
-        
 
     def get_filtered_image(self):
         """Return filtered image."""
@@ -146,8 +146,9 @@ class SobelFilter:
 
 
 @click.command()
-@click.argument(image_file)
+@click.argument('image_file')
 def main(image_file):
+    """Run Sobel Filter."""
     input_dir = "input"
     output_dir = "output"
 
@@ -157,7 +158,8 @@ def main(image_file):
         print("Error: Input Directory does not exist")
         exit(1)
     if not os.path.isfile(input_dir + '/' + image_file):
-        print("Error: Image file " + image_file + " does not exist in input folder.")
+        print("Error: Image file "
+              + image_file + " does not exist in input folder.")
         exit(1)
 
     image_in = imageio.imread(input_dir + '/' + image_file)
@@ -167,7 +169,7 @@ def main(image_file):
     image_out = sobel.get_filtered_image()
 
     if not os.path.exists(output_dir):
-        os.makedir(output_dir)
+        os.makedirs(output_dir)
 
     file_split = image_file.split('.')
     output_filename = file_split[0] + '_sob.' + file_split[1]
