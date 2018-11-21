6b8ea0e4ee7f020a916721675f28403687feda5b
Colin Page <cwpage@umich.edu>
2018-11-20 23:16:06 -0500

    semantics

--- sobelfilter/helper.py
+++ sobelfilter/helper.py
@@ -3,6 +3,6 @@ def progressBar(current_iteration, total_iterations, decimals=2, length=100, fil
 	percent = ("{0:." + str(decimals) + "f}").format(100*(current_iteration/float(total_iterations)))
 	filled_length = int(length * current_iteration // total_iterations)
 	bar = fill * filled_length + '_' * (length - filled_length)
-	print('\rProgress |%s %s%%' % (bar, percent), end = '\r')
+	print('\rProgress: |%s %s%%' % (bar, percent), end = '\r')
 	if current_iteration == total_iterations:
 		print()
