d81b72918ca860c1cc6f22906d867857f0761d92
Colin Page <cwpage@umich.edu>
2018-11-20 20:49:42 -0500

    updated progressbar character

--- sobelfilter/helper.py
+++ sobelfilter/helper.py
@@ -1,5 +1,5 @@
 """Helper File."""
-def progressBar(current_iteration, total_iterations, decimals=2, length=100, fill=chr(220)):
+def progressBar(current_iteration, total_iterations, decimals=2, length=100, fill=u"\u2588"):
 	percent = ("{0:." + str(decimals) + "f}").format(100*(current_iteration/float(total_iterations)))
 	filled_length = int(length * current_iteration // total_iterations)
 	bar = fill * filled_length + '_' * (length - filled_length)
